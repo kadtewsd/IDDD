7c5be2108e133e830eebd0880c062d1cc9e0a25a
kasakaid <kasakaid@outlook.com>
2018-08-04 23:29:49 +0900

    Domain Knowledge is fluxed. so, Domain Service is created and account is able to draw money

--- src/main/kotlin/domain/model/financial/Account.kt
+++ src/main/kotlin/domain/model/financial/Account.kt
@@ -1,3 +1,13 @@
 package domain.model.financial
 
-data class Account(val number: AccountNumber, val bank: Bank, val balance: Int)
+import domain.model.Money
+
+data class Account(val number: AccountNumber, val bank: Bank, val balance: Int) {
+
+    fun draw(money: Money): Account {
+        if (this.balance < money.value) {
+            throw BalanceNotMatchedException("残金より大きな金額を請求できません")
+        }
+        return this.copy(balance = this.balance - money.value)
+    }
+}
